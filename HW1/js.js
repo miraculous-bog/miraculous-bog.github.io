2ac0f150e34af72042294e23054df0f0c55bc577
miraculous-bog <55063523+miraculous-bog@users.noreply.github.com>
2020-07-24 18:29:58 +0300

    Update js.js

--- HW1/js.js
+++ HW1/js.js
@@ -1,12 +1,12 @@
-const Prod_Apple = 15.678;
-	const Prod_Cherry = 123.965;
-	const Prod_Pine = 90.2345;
+const PRODE_APPLE = 15.678;
+	const PROD_CHERRY = 123.965;
+	const PROD_PINE = 90.2345;
 
-	console.log("Максимальна ціна" + Math.max(Prod_Apple,Prod_Cherry,Prod_Pine));
-	console.log("Мінімальна ціна" + Math.min(Prod_Apple,Prod_Cherry,Prod_Pine));
+	console.log("Максимальна ціна" + Math.max(PRODE_APPLE,PROD_CHERRY,PROD_PINE));
+	console.log("Мінімальна ціна" + Math.min(PRODE_APPLE,PROD_CHERRY,PROD_PINE));
 
 	let sum;
-	sum = Prod_Apple + Prod_Cherry + Prod_Pine;
+	sum = PRODE_APPLE + PROD_CHERRY + PROD_PINE;
 	console.log("Сума цін на продукти" + sum);
 
 	let prodAppleV = Math.floor(15.678);
@@ -22,16 +22,16 @@ const Prod_Apple = 15.678;
 		console.log("чи парне число" + true);
 	} else { console.log("чи парне число" + false)};
 
-	const  Money_Gave = 500;
-	console.log("ваша решта" + Money_Gave - sum);
+	const  MONEY_GAVE = 500;
+	console.log("ваша решта" + (MONEY_GAVE - sum));
 
 	console.log("середнє та округлене значення ціни" + (sum/3).toFixed(2));
 
 	let discount = Math.random(10)*100;
 	console.log("ваша знажка" + discount.toFixed(0));
-	let marga = Prod_Cherry / 2;
-	let sale = (Prod_Cherry / 100) * discount;
-	let price = Prod_Cherry - discount;
+	let marga = PROD_CHERRY / 2;
+	let sale = (PROD_CHERRY / 100) * discount;
+	let price = PROD_CHERRY - discount;
 	console.log("ціна із знижкою" + price.toFixed(2));
-	let margaEnd = Prod_Cherry - marga - sale;
+	let margaEnd = PROD_CHERRY - marga - sale;
 	console.log("прибуток" + margaEnd.toFixed(2));
