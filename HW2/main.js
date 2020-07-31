ac52facf3688b6d335451a9d0fce1cce2320ef2a
miraculous-bog <55063523+miraculous-bog@users.noreply.github.com>
2020-07-31 11:21:05 +0300

    Update main.js

--- HW2/main.js
+++ HW2/main.js
@@ -1,8 +1,8 @@
  let first = Number(prompt('enter first number'));
 let	second = Number(prompt('enter second number'));
 
-while ((!Number.isInteger(first) || !Number.isInteger(second))
- || second<first)  {
+while (((!Number.isInteger(first) || !Number.isInteger(second))
+ || second<first) || (first=='' && second == ''))  {
  	if (second<first) {
 	first = Number(prompt('enter first number,second>first!'));
 	second = Number(prompt('enter second number,second>first!'));}
@@ -11,14 +11,14 @@ while ((!Number.isInteger(first) || !Number.isInteger(second))
 	second = Number(prompt('enter second number'));}
 } 
 
-let even = confirm('do you need scip even numbers');
+const EVEN = confirm('do you need scip even numbers');
 let sum = first;
 
 
 function pl(num) { return ((num % 2) == 0 ? c = true : c=false);}
 
 
-	if (!even) {
+	if (!EVEN) {
 		for (i = first; i<=second; i++ ) {
 				sum = sum + i;			 
 		}
@@ -30,19 +30,3 @@ function pl(num) { return ((num % 2) == 0 ? c = true : c=false);}
 		} 
 
 console.log(sum-1);
-
-
-/*function isInt(num) {
-if ( parseInt( num ) != num ) {
-    a = false;
-} else {
-    a = true;
-} return a;
-}
-
-let first;
-let second;
-do { 
-	first = parseInt(prompt('enter first number'));
-	second = parseInt(prompt('enter second number'));
-} while(isInt(first) & isInt(second));*/
