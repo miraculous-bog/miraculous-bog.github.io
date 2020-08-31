3ff8735650e654787fd35fd4b32afbcf55b323c2
miraculous-bog <55063523+miraculous-bog@users.noreply.github.com>
2020-09-01 01:13:58 +0300

    site

---
+++ lending of liquid/js/js.js
@@ -0,0 +1,7 @@
+  $('.sl').slick({
+	  autoplay: true,
+	  autoplaySpeed: 1500,
+	  dots: true,
+	  fade: true,
+	  zIndex: 1
+  });
---
+++ lending of liquid/js/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#trade").owlCarousel({
+    items:4,
+    margin:20, /*по желанию*/
+    nav:true,/*стрелки*/
+	loop: true,
+    autoplay:true,  /*по желанию*/
+	   autoplayTimeout: 4000,  /*по желанию*/
+	pagination: true,
+	paginationNumbers: true
+  });
+});
+
+
