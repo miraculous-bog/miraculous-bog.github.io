a94c726e30699c7f179ecb74af20d6102e2725c4
miraculous-bog <55063523+miraculous-bog@users.noreply.github.com>
2020-09-08 17:41:23 +0300

    ц

--- HW5/main.js
+++ HW5/main.js
@@ -1,4 +1,4 @@
-//(1)функція повертає масив випадкових чисел із заданою довжиною та числовими параметрами
+/*//(1)функція повертає масив випадкових чисел із заданою довжиною та числовими параметрами
 function f(arr,min,max){
 	let arrNew = [];
 	min = Math.ceil(min);
@@ -56,4 +56,63 @@ console.log(filterEvenNumbers);
  const getDividedByFive = (...numbers) => {
  	return numbers.filter((el) => (el % 5)==0);
  }
- console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+ console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));*/
+
+
+
+/*let numbers = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2];
+
+function getModa(nums) {
+	let newArr = [];
+	newArr = nums.forEach((el) => {
+	let a = el;
+    	if (newArr[a] != undefined)
+			newArr[a]++;
+		else 
+			newArr[a] = 1;
+	});
+	  	 return newArr;
+}
+console.log(getModa(numbers));
+*/
+arr = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2, 3,3,3,3];
+
+function arrayCountValues(arr) {
+    var v, freqs = {};
+
+    for (var i = arr.length; i--; ) { 
+        v = arr[i];
+        if (freqs[v]) freqs[v] += 1;
+        else freqs[v] = 1;
+    }
+/*    var max = 0;
+
+for (let property in freqs) {
+  max = (max < parseFloat(property)) ? parseFloat(property) : max;
+}*/
+/*return Object.keys(freqs).reduce(function(a, b){ return freqs[a] > freqs[b] ? a : b });*/
+/*	let max = Math.max.apply(null,freqs); return max;*/
+	for(var i in freqs){
+		if(freqs[i] == 5){
+			return i;
+		}
+	}
+}
+
+console.log(arrayCountValues(arr));
+
+function divideWord(word) {
+	arr= [];
+	for(let i = 0; i<word.length; i=i+3) {
+		let three = word.slice(i,i+3);
+		arr.push(three);
+	}
+	return arr;
+}
+
+console.log(divideWord("ya_carshedfsdvvvvvvvvagrzfvawekrjmfnd"));
+
+
+
+
+
