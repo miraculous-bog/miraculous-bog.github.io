9e3cbfe428333dcbe9760e0f65593c7743884d27
miraculous-bog <55063523+miraculous-bog@users.noreply.github.com>
2020-11-22 19:51:29 +0200

    орп

--- slider/js/main.js
+++ slider/js/main.js
@@ -24,6 +24,29 @@ g.onmousemove = function(e) {
 $(".arrow-4").click(function() {
     $(this).toggleClass("open");
 });
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 50, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
 /*var slideshowInfo = document.getElementById('slideshowInfo');
 		var slidesInfo = slideshowInfo.getElementsByTagName('img');
 		var index = 0; 
@@ -213,3 +236,15 @@ Sim.initialize = function(that) {
 new Sim();
 
 
+$(document).ready(function() {
+   var margin = -100; // переменная для контроля докрутки
+   $("a.scroll").click(function() { // тут пишите условия, для всех ссылок или для конкретных
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top+margin+ "px" // .top+margin - ставьте минус, если хотите увеличить отступ
+      }, {
+         duration: 1300, // тут можно контролировать скорость
+         easing: "swing"
+      });
+      return false;
+   });
+});
