60f11d4a1dda8c784e47696faf5f7cd053663ac6
miraculous-bog <55063523+miraculous-bog@users.noreply.github.com>
2020-12-02 14:16:51 +0200

    new project (edited slider-controlers))

--- project-edited/js/head.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.header_burger').click(function(event){
-		$('.header_burger,.header_menu').toggleClass('activem');
-		$('.down').toggleClass('activemc');
-		$('body').toggleClass('lockm');
-	});
-});
