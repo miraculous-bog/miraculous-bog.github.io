ff29a57dcfbfb5cefc3b1b2e19d28bed481482bb
miraculous-bog <55063523+miraculous-bog@users.noreply.github.com>
2021-01-17 05:59:04 +0200

    12432

--- new-version3/js/bmain.js
+++ new-version3/js/bmain.js
@@ -65,42 +65,7 @@ function addToCart(event) {
     //добавляем товар в корзину
     event.preventDefault();
 
-/*  var carting = $('.basketImg');
-  var imgtodrag = $(this).parent('.trade-block').find("img").eq(0);
-  if (imgtodrag) {
-  var imgclone = imgtodrag.clone()
-  .offset({
-  top: imgtodrag.offset().top,
-  left: imgtodrag.offset().left
-  })
-  .css({
-  'opacity': '0.5',
-  'position': 'absolute',
-  'height': '150px',
-  'width': '150px',
-  'z-index': '100'
-  })
-  .appendTo($('body'))
-  .animate({
-  'top': carting.offset().top + 10,
-  'left': carting.offset().left + 10,
-  'width': 75,
-  'height': 75
-  }, 1000, 'easeInOutExpo');
-   
-  setTimeout(function () {
-  carting.effect("shake", {
-  times: 2
-  }, 200);
-  }, 1500);
-
-  imgclone.animate({
-  'width': 0,
-  'height': 0
-  }, function () {
-  $(this).detach()
-  });
-  }
+/* 
 */
     var id = $(this).attr('data-id');
     // console.log(id);
